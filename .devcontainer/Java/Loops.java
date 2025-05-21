020240018cae1a1d62d8b646d241ecc5b5bd78a0
Sumit Sharma <xumit97575235@gmail.com>
2025-05-21 09:56:35 +0000

    some loops

---
+++ .devcontainer/Java/Loops.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("JAI SHREE RAM");
+        // FOR LOOPS
+        for (int i = 0; i<5; i++){
+            System.out.println("Ram Ram ji");
+        }
+        
+        // print 1 to 10 using for loops
+
+        for (int i = 1; i<=10; i++){
+            System.out.print(i + " ");
+        }
+
+        //  find largest nujmber 
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 0; i<number; i++){
+            int a = sc.nextInt();
+        }
+
+
+        
+    }
+}
